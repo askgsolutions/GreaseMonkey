887c3df3097f7ef7125282a64d6c0043aa32f558
Arunselvakumar Govindaraj <arunselvakumar.g@gmail.com>
2020-04-07 22:48:16 +0530

    Initial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,NgZone } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
+declare var google: any;
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,67 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  GoogleAutocomplete: any;
+  geocoder: any;
+  autocomplete: { input: string; };
+  autocompleteItems: any[];
+  nearbyItems: any[];
 
-  constructor() {}
+  constructor(public zone: NgZone, public geolocation: Geolocation) {
+    this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
+    this.geocoder = new google.maps.Geocoder;
+    this.autocomplete = { input: '' };
+    this.autocompleteItems = [];
+  }
 
+  searchLocations(){
+    if (this.autocomplete.input == '') {
+      this.autocompleteItems = [];
+      return;
+    }
+    this.GoogleAutocomplete.getPlacePredictions({ input: this.autocomplete.input },
+    (predictions, status) => {
+      this.autocompleteItems = [];
+      this.zone.run(() => {
+        predictions.forEach((prediction) => {
+          this.autocompleteItems.push(prediction);
+        });
+      });
+    });
+  }
+
+  currentLocation(){
+    this.geolocation.getCurrentPosition().then((position) => {
+      let latLong = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      };
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+
+  getSearchResult(item){
+    this.autocompleteItems = [];
+  
+    this.geocoder.geocode({'placeId': item.place_id}, (results, status) => {
+      if(status === 'OK' && results[0]){
+        this.autocompleteItems = [];
+        // this.GooglePlaces.nearbySearch({
+        //   location: results[0].geometry.location,
+        //   radius: '500',
+        //   types: ['restaurant'],
+        //   // key: 'YOUR_KEY_HERE'
+        // }, (near_places) => {
+        //     this.zone.run(() => {
+        //       this.nearbyItems = [];
+        //       for (var i = 0; i < near_places.length; i++) {
+        //         this.nearbyItems.push(near_places[i]);
+        //       }
+        //   });
+        // })
+      }
+    })
+    console.log(this.autocompleteItems);
+  }
 }
